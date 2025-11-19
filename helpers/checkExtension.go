96d373850c051ceed6ad09b2366b058ad23d8e49
drawliin <itshoussameddine@gmail.com>
2025-11-20 00:45:41 +0100

    cleaning extension finder

--- helpers/checkExtension.go
+++ helpers/checkExtension.go
@@ -1,21 +1,20 @@
 package helper
 
 import (
+	"slices"
 	"strings"
 )
 
 func CheckExtension(s string) bool {
 	invalidExtensions := []string{"go", "mod", "sum"}
-	
+
 	fields := strings.Split(s, ".")
-	
+
 	extension := fields[len(fields)-1]
-	
-	for _, ext := range invalidExtensions {
-		if ext == extension {
-			return false
-		}
+
+	if slices.Contains(invalidExtensions, extension) {
+		return false
 	}
-	
+
 	return true
 }
