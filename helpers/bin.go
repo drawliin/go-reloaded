a14d6e07ce476ea15279f17a59b8b8be9054163d
drawliin <boomepic99@gmail.com>
2025-11-20 11:33:40 +0100

    handling bin

--- helpers/bin.go
+++ helpers/bin.go
@@ -12,8 +12,14 @@ func binary(c rune) (int, bool) {
 		return 0, true
 	}
 }
+
 func Bin(s string) string {
 	num := 0
+	sign := ""
+	if s[0] == '-' {
+		s = s[1:]
+		sign = "-"
+	}
 	for _, c := range s {
 		d, err := binary(c)
 		if err {
@@ -21,5 +27,5 @@ func Bin(s string) string {
 		}
 		num = num*2 + d
 	}
-	return strconv.Itoa(num)
+	return sign+strconv.Itoa(num)
 }
